8574c5655f8f364a9f9d3ff0207a5e2906286eff
Stas Baranovskiy <stanislav.baranovskiy@cactussoft.biz>
2015-03-19 12:27:10 +0300

    basic structure

---
+++ SBViperCount/AppDependencies.swift
@@ -0,0 +1,26 @@
+//
+//  AppDependencies.swift
+//  SBViperCount
+//
+//  Created by Stas Baranovskiy on 3/19/15.
+//  Copyright (c) 2015 Stas Baranovskiy. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AppDependencies {
+
+    
+    init() {
+        configureDependencies()
+    }
+        
+    func installRootViewControllerIntoWindow(window: UIWindow) {
+        
+    }
+    
+    func configureDependencies() {
+        
+    }
+}
